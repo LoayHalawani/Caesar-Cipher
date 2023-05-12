30dde8429a5f527498b62a2c9b68bb2f26ec86bc
Loay Halawani <81193505+loayhalawani@users.noreply.github.com>
2023-05-12 05:28:05 +0300

    Update CaeserCipher.java

--- CaeserCipher.java
+++ CaeserCipher.java
@@ -1,13 +1,17 @@
 import java.util.*;
 
-public class CaeserCipher {
-	static int computeShift(char key) {
+public class CaeserCipher
+{
+	static int computeShift(char key)
+	{
 		char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		key = Character.toLowerCase(key);
 
 		int shift = 0;
-		for(int i = 0; i < alpha.length; i ++) {
-			if(key == alpha[i]) {
+		for(int i = 0; i < alpha.length; i ++)
+		{
+			if(key == alpha[i])
+			{
 				shift = i;
 				break;
 			}
@@ -16,21 +20,28 @@ public class CaeserCipher {
 		return shift;
 	}
 
-	static String encrypt(String input, int shift) {
+	static String encrypt(String input, int shift)
+	{
 		char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		
 		String output = "";
-		for(int i = 0; i < input.length(); i ++) {
+		for(int i = 0; i < input.length(); i ++)
+		{
 			char temp = ' ';
-			if(Character.isUpperCase(input.charAt(i))) {
+			if(Character.isUpperCase(input.charAt(i)))
+			{
 				temp = Character.toLowerCase(input.charAt(i));
-				for(int j = 0; j < alpha.length; j ++) {
-					if(temp == alpha[j]) {
-						if(j + shift > alpha.length - 1) {
+				for(int j = 0; j < alpha.length; j ++)
+				{
+					if(temp == alpha[j])
+					{
+						if(j + shift > alpha.length - 1)
+						{
 							int k = (j + shift) - (alpha.length - 1) - 1;
 							temp = alpha[k];
 						}
-						else {
+						else
+						{
 							temp = alpha[j + shift];
 						}
 						break;
@@ -38,15 +49,20 @@ public class CaeserCipher {
 				}
 				output += Character.toUpperCase(temp);
 			}
-			else if(Character.isLowerCase(input.charAt(i))) {
+			else if(Character.isLowerCase(input.charAt(i)))
+			{
 				temp = input.charAt(i);
-				for(int j = 0; j < alpha.length; j ++) {
-					if(temp == alpha[j]) {
-						if(j + shift > alpha.length - 1) {
+				for(int j = 0; j < alpha.length; j ++)
+				{
+					if(temp == alpha[j])
+					{
+						if(j + shift > alpha.length - 1)
+						{
 							int k = (j + shift) - (alpha.length - 1) - 1;
 							temp = alpha[k];
 						}
-						else {
+						else
+						{
 							temp = alpha[j + shift];
 						}
 						break;
@@ -54,7 +70,8 @@ public class CaeserCipher {
 				}
 				output += temp;
 			}
-			else {
+			else
+			{
 				output += input.charAt(i);
 			}
 		}
@@ -62,21 +79,28 @@ public class CaeserCipher {
 		return output;
 	}
 
-	static String decrypt(String input, int shift) {
+	static String decrypt(String input, int shift)
+	{
 		char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		
 		String output = "";
-		for(int i = 0; i < input.length(); i ++) {
+		for(int i = 0; i < input.length(); i ++)
+		{
 			char temp = ' ';
-			if(Character.isUpperCase(input.charAt(i))) {
+			if(Character.isUpperCase(input.charAt(i)))
+			{
 				temp = Character.toLowerCase(input.charAt(i));
-				for(int j = 0; j < alpha.length; j ++) {
-					if(temp == alpha[j]) {
-						if(j - shift < 0) {
+				for(int j = 0; j < alpha.length; j ++)
+				{
+					if(temp == alpha[j])
+					{
+						if(j - shift < 0)
+						{
 							int k = (j - shift) + (alpha.length - 1) + 1;
 							temp = alpha[k];
 						}
-						else {
+						else
+						{
 							temp = alpha[j - shift];
 						}
 						break;
@@ -84,15 +108,20 @@ public class CaeserCipher {
 				}
 				output += Character.toUpperCase(temp);
 			}
-			else if(Character.isLowerCase(input.charAt(i))) {
+			else if(Character.isLowerCase(input.charAt(i)))
+			{
 				temp = input.charAt(i);
-				for(int j = 0; j < alpha.length; j ++) {
-					if(temp == alpha[j]) {
-						if(j - shift < 0) {
+				for(int j = 0; j < alpha.length; j ++)
+				{
+					if(temp == alpha[j])
+					{
+						if(j - shift < 0)
+						{
 							int k = (j - shift) + (alpha.length - 1) + 1;
 							temp = alpha[k];
 						}
-						else {
+						else
+						{
 							temp = alpha[j - shift];
 						}
 						break;
@@ -100,7 +129,8 @@ public class CaeserCipher {
 				}
 				output += temp;
 			}
-			else {
+			else
+			{
 				output += input.charAt(i);
 			}
 		}
@@ -108,7 +138,8 @@ public class CaeserCipher {
 		return output;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("\nPlease enter input to cipher/decipher: ");
@@ -117,7 +148,8 @@ public class CaeserCipher {
 		System.out.println("\nPlease enter key letter: ");
 		String input2 = scan.next();
 		char key = input2.charAt(0);
-		if(!Character.isLetter(key) || input2.length() > 1) {
+		if(!Character.isLetter(key) || input2.length() > 1)
+		{
 			System.err.println("\nERROR: Invalid key letter! Please try again.");
 			System.exit(0);
 		}
@@ -128,15 +160,18 @@ public class CaeserCipher {
 		char ans = scan.next().charAt(0);
 		
 		String output = "";
-		if(Character.toLowerCase(ans) == 'e') {
+		if(Character.toLowerCase(ans) == 'e')
+		{
 			output = encrypt(input, shift);
 			System.out.println("\nCipher: " + output);
 		}
-		else if(Character.toLowerCase(ans) == 'd') {
+		else if(Character.toLowerCase(ans) == 'd')
+		{
 			output = decrypt(input, shift);
 			System.out.println("\nDecipher: " + output);
 		}
-		else {
+		else
+		{
 			System.err.println("\nERROR: Invalid choice letter! Please try again.");
 			System.exit(0);
 		}
